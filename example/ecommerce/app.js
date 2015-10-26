3d58a8394310bf36b45dd5d0dc60213122d6e8eb
Pixelastic <tim@pixelastic.com>
2015-10-26 16:39:51 +0100

    Temp theming (skippin npm run lint)

---
+++ example/ecommerce/app.js
@@ -0,0 +1,145 @@
+// force using index because package 'main' is dist/
+var instantsearch = require('../../index');
+
+var search = instantsearch({
+  appId: 'latency',
+  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
+  indexName: 'instant_search',
+  urlSync: {
+    useHash: true
+  }
+});
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#search-box',
+    placeholder: 'Search for products',
+    poweredBy: true
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.stats({
+    container: '#stats'
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.indexSelector({
+    container: '#index-selector',
+    indices: [
+      {name: 'instant_search', label: 'Most relevant'},
+      {name: 'instant_search_price_asc', label: 'Lowest price'},
+      {name: 'instant_search_price_desc', label: 'Highest price'}
+    ]
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      empty: require('./templates/no-results.html'),
+      item: require('./templates/item.html')
+    },
+    hitsPerPage: 6
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+    maxPages: 20
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.refinementList({
+    container: '#brands',
+    facetName: 'brand',
+    operator: 'or',
+    limit: 10,
+    templates: {
+      header: 'Brands'
+    }
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.refinementList({
+    container: '#price-range',
+    facetName: 'price_range',
+    operator: 'and',
+    limit: 10,
+    templates: {
+      header: 'Price ranges'
+    },
+    transformData: function(data) {
+      data.name = data.name.replace(/(\d+) - (\d+)/, '$$$1 - $$$2').replace(/> (\d+)/, '> $$$1');
+      return data;
+    }
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.toggle({
+    container: '#free-shipping',
+    facetName: 'free_shipping',
+    label: 'Free Shipping',
+    templates: {
+      header: 'Shipping'
+    }
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.menu({
+    container: '#categories',
+    facetName: 'categories',
+    limit: 10,
+    templates: {
+      header: 'Categories'
+    }
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.rangeSlider({
+    container: '#price',
+    facetName: 'price',
+    templates: {
+      header: 'Price'
+    },
+    tooltips: {
+      format: function(formattedValue) {
+        return '$' + formattedValue;
+      }
+    }
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.hierarchicalMenu({
+    container: '#hierarchical-categories',
+    attributes: ['hierarchicalCategories.lvl0', 'hierarchicalCategories.lvl1', 'hierarchicalCategories.lvl2'],
+    templates: {
+      header: 'Hierarchical categories'
+    }
+  })
+);
+
+// search.once('render', function() {
+//   document.querySelector('.search').className = 'row search search--visible';
+// });
+
+search.addWidget(
+  instantsearch.widgets.priceRanges({
+    container: '#price-ranges',
+    facetName: 'price',
+    templates: {
+      header: 'Price ranges'
+    }
+  })
+);
+
+search.start();
